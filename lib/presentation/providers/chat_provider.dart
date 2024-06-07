862d06b36b5436da933101fcec6df6a77261d5e0
Carlos Catalan <carlosdanielcatalan1o@gmail.com>
2024-06-06 18:57:33 -0700

    Feat(GetYesNOAnswer): Making request to YesNo API with dio libary

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,10 +1,12 @@
 
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier{
 
   final ScrollController chatScrollController = ScrollController();
+  final getYesNoAnswer = GetYesNoAnswer();
 
   List<Message> messageList = [
     Message(text: 'Hola Amor!!', fromWho: FromWho.mine),
@@ -16,6 +18,17 @@ class ChatProvider extends ChangeNotifier{
     final newMessage = Message(text: text, fromWho: FromWho.mine);
     messageList.add(newMessage);
 
+    if(text.endsWith('?')){
+      herReply();
+    }
+
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> herReply() async{
+    final herMessage = await getYesNoAnswer.getAnswer();
+    messageList.add(herMessage);
     notifyListeners();
     moveScrollToBottom();
   }
@@ -28,5 +41,5 @@ class ChatProvider extends ChangeNotifier{
       duration: const Duration(milliseconds: 300), 
       curve: Curves.easeOut
     );
-  }
 }
+  }
